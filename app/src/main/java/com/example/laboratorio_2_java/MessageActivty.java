a6e401eac2fab0117c515a307f6907f019924dfc
William Arias <willi.am@hotmail.es>
2022-05-17 20:40:41 -0600

    Can access final Activity through Message Activity.

--- app/src/main/java/com/example/laboratorio_2_java/MessageActivty.java
+++ app/src/main/java/com/example/laboratorio_2_java/MessageActivty.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MessageActivty extends AppCompatActivity {
@@ -19,5 +20,10 @@ public class MessageActivty extends AppCompatActivity {
 
         //comments for testing.
 
+    }
+
+    public void showActivity(View view) {
+        Intent intent = new Intent(this, FinalActivity.class);
+        startActivity(intent);
     }
 }
